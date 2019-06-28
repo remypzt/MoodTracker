11f59f9b0a1c25a029482e5951a9850baf0f1cf1
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-06-28 16:06:38 +0200

    Optimize code

--- app/src/main/java/remy/pouzet/moodtracker/controller/MainActivity.java
+++ app/src/main/java/remy/pouzet/moodtracker/controller/MainActivity.java
@@ -1,18 +1,18 @@
 package remy.pouzet.moodtracker.controller;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import remy.pouzet.moodtracker.model.OnSwipeTouchListener;
 import remy.pouzet.moodtracker.R;
+import remy.pouzet.moodtracker.model.OnSwipeTouchListener;
 
 public class MainActivity extends AppCompatActivity
 
 {
-    private LinearLayout mBackground;
+
     private ImageView mBackgroundImage;
 
 
@@ -24,57 +24,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* mBackground = findViewById(R.id.activity_main_background) ;*/
 
+        mBackgroundImage = findViewById(R.id.imageView);
 
-       /* ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new CustomerPageAdaptater(this));
-
-
-        mBackground = findViewById(R.id.activity_main_background);
-
-       /* mBackground.setBackgroundColor(getResources().getColor(R.color.light_sage));*/
-
-       mBackgroundImage = findViewById(R.id.imageView);
-
-       mBackgroundImage.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
-            public void onSwipeTop() {
-                /*mBackground.setBackground;*/
-
+        mBackgroundImage.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this)
+        {
+            public void onSwipeTop()
+            {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_LONG).show();
             }
-            public void onSwipeBottom() {
 
-                /*setContentView(R.layout.activity_main_1);*/
+            public void onSwipeBottom()
+            {
 
             }
 
         });
-       /*if Content View (or mbackground  or mBackground image 2 )= color1 then if OnSwipeTop then color 2 and if Onswipebottom then color 1 etc ()*/
-       {}
     }
-    /* custompage adaptater//
-    public enum CustomPagerEnum
-    {
-
-        RED(R.string.app_name, R.layout.activity_main_1);
-
-
-        private int mTitleResId;
-        private int mLayoutResId;
-
-        CustomPagerEnum(int titleResId, int layoutResId) {
-            mTitleResId = titleResId;
-            mLayoutResId = layoutResId;
-        }
-
-        public int getTitleResId() {
-            return mTitleResId;
-        }
-
-        public int getLayoutResId() {
-            return mLayoutResId;
-        }
-
-    }*/
 
 }
