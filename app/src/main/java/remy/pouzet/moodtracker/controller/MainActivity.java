743bb4ad943c04dfd8f2600a518011505e68e803
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-06-28 16:13:05 +0200

    Xml background bug corrections and swipe test erase

--- app/src/main/java/remy/pouzet/moodtracker/controller/MainActivity.java
+++ app/src/main/java/remy/pouzet/moodtracker/controller/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
         {
             public void onSwipeTop()
             {
-                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_LONG).show();
+                
             }
 
             public void onSwipeBottom()
