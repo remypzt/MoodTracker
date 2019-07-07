09e0278eba85550c5e5cf1d6ed50ae7f68c07a77
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-07-07 14:59:32 +0200

    - Build get moods in historicActivity

--- app/src/main/java/remy/pouzet/moodtracker/controller/HistoricActivity.java
+++ app/src/main/java/remy/pouzet/moodtracker/controller/HistoricActivity.java
@@ -1,22 +1,43 @@
 package remy.pouzet.moodtracker.controller;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+
 import remy.pouzet.moodtracker.R;
+import remy.pouzet.moodtracker.model.Mood;
+
 
 /**
  * Created by Remy Pouzet on 29/06/2019.
  */
 public class HistoricActivity extends AppCompatActivity
 {
+
+    public static final String PREF_KEY_MOOD = "PREF_KEY_MOOD";
+    private SharedPreferences mPreferences;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historic_activity);
+
+        // Get moods
+        mPreferences = getPreferences(MODE_PRIVATE);
+        String fromJsonMoods = mPreferences.getString(PREF_KEY_MOOD, null);
+        Gson gson = new Gson();
+        ArrayList<Mood> moods = gson.fromJson(fromJsonMoods, new TypeToken<ArrayList<Mood>>()
+        {
+        }.getType());
+        //END\| Get moods
+
 
 
     }
